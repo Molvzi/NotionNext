462c496df12c4d03f37e06f0c2344fcb4f63cf6d
Molvzi <116082154+Molvzi@users.noreply.github.com>
2024-05-02 15:10:05 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'XIAOLV.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_4: '上线',
+  HEO_HERO_TITLE_5: '小绿',
   HEO_HERO_TITLE_LINK: 'https://xiaolv.com',
 
   // 英雄区显示三个置顶分类
